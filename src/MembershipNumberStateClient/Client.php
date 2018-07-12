1e59601911138dd914d3665fd532041d2c7fc4f0
Jatinder.Aietan <jatinder.aietan@tastecard.co.uk>
2018-07-12 11:59:30 +0100

    AT-532 update the activate function to accept an array of membership numbers and expirydates rather than single arguments.

--- src/MembershipNumberStateClient/Client.php
+++ src/MembershipNumberStateClient/Client.php
@@ -4,8 +4,11 @@ namespace Dcg\Client\MembershipNumberStateClient;
 
 class Client
 {
-    public function activate($membershipNumber, $expiryDate)
+    public function activate(array $membershipData)
     {
         //@TODO
+        // send a message to the the membership number state service api
+        // expect good things back.
+        return true;
     }
 }
