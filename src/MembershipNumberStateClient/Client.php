0f196ec1d7e16ee1160b806c2de84c21051b48ff
Jatinder.Aietan <jatinder.aietan@tastecard.co.uk>
2018-07-13 16:38:20 +0100

    AT-385 client lib updated the endpoint for now

--- src/MembershipNumberStateClient/Client.php
+++ src/MembershipNumberStateClient/Client.php
@@ -5,8 +5,28 @@ namespace Dcg\Client\MembershipNumberState;
 use Dcg\Client\MembershipNumberState\Utils\Api;
 use Dcg\Client\MembershipNumberState\Utils\Dates;
 
+
 class Client
 {
+    private function getEndPoint($endPointName){
+        $baseUrl = 'https://member-num-state.test.diningclubgroup.com/v1';
+
+        switch($endPointName){
+            case 'MembershipNumberStateActivateApiEndpoint':
+                $ep = '/activate';
+                break;
+            case 'MembershipNumberStateDeactivateApiEndpoint':
+                $epp = '/deactivate';
+                break;
+            case 'MembershipNumberStateUpdateExpiryDateApiEndpoint':
+                $ep = '/update';
+                break;
+            default:
+                throw new \Exception("unknown endpoint requested",401);
+        }
+        return $baseUrl.$ep;
+    }
+
     public function activate(array $membershipData)
     {
         /* we are expecting data in the format:
@@ -19,7 +39,7 @@ class Client
                 }
             }
         }
-        $membershipNumberStateApiEndpoint = env('MembershipNumberStateActivateApiEndpoint');
+        $membershipNumberStateApiEndpoint = $this->getEndpoint('MembershipNumberStateActivateApiEndpoint');
         $requestResponse = Api::sendRequest([],$membershipNumberStateApiEndpoint,'POST',$membershipData);
 
         return  $requestResponse['successful'];
@@ -37,7 +57,7 @@ class Client
                 }
             }
         }
-        $membershipNumberStateApiEndpoint = env('MembershipNumberStateDeactivateApiEndpoint');
+        $membershipNumberStateApiEndpoint = $this->getEndPoint('MembershipNumberStateDeactivateApiEndpoint');
         $requestResponse = Api::sendRequest([],$membershipNumberStateApiEndpoint,'POST',$membershipData);
 
         return  $requestResponse['successful'];
@@ -55,7 +75,7 @@ class Client
                 }
             }
         }
-        $membershipNumberStateApiEndpoint = env('MembershipNumberStateUpdateExpiryDateApiEndpoint');
+        $membershipNumberStateApiEndpoint = $this->getEndPoint('MembershipNumberStateUpdateExpiryDateApiEndpoint');
         $requestResponse = Api::sendRequest([],$membershipNumberStateApiEndpoint,'POST',$membershipData);
 
         return  $requestResponse['successful'];
