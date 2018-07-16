d3f849fb30001e6a84e351a015394966fd92a73f
gulzaman.khan <gulzaman.khan@tastecard.co.uk>
2018-07-16 15:07:50 +0100

    config params

--- src/MembershipNumberStateClient/Config/FileCreator.php
+++ src/MembershipNumberStateClient/Config/FileCreator.php
@@ -4,18 +4,22 @@ namespace Dcg\Client\MembershipNumberState\Config;
 
 class FileCreator {
 
-    /**
-     *  Copy package's config file to project
-     */
-    public static function createConfigFile ($event)
-    {
+	/**
+	 *  Copy package's config file to project
+	 */
+	public static function createConfigFile ($event)
+	{
 		$vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
-		$configFile = realpath($vendorDir.'/../config/membership-number-state-client.php');
-		$exampleFile = $vendorDir.'/dcg/dcg-lib-membership-number-state-client/config.php';
+		$configDir = dirname($vendorDir). DIRECTORY_SEPARATOR . 'config';
+		$configFile = $configDir . DIRECTORY_SEPARATOR . 'membership-number-state-client.php';
+		$exampleFile = $vendorDir . DIRECTORY_SEPARATOR . 'dcg'. DIRECTORY_SEPARATOR .'dcg-lib-membership-number-state-client' . DIRECTORY_SEPARATOR . 'config.php';
 
-        if (!file_exists($configFile)) {
-            copy($exampleFile, $configFile);
-        }
-    }
+		if (!file_exists($configDir)) {
+			mkdir($configDir);
+		}
+		if (!file_exists($configFile)) {
+			copy($exampleFile, $configFile);
+		}
+	}
 
 }
