3f26091ef9dd8909fbfb1192940332abc5a9b1ac
gulzaman.khan <gulzaman.khan@tastecard.co.uk>
2018-07-19 13:28:02 +0100

    AT-575 moved config to a separate package

--- src/MembershipNumberStateClient/Config/FileCreator.php
+++ src/MembershipNumberStateClient/Config/FileCreator.php
@@ -6,20 +6,20 @@ class FileCreator extends \Dcg\Config\FileCreator
 {
     /**
      * Get the location of the config file to use as an example (template)
-     * @param Composer\Installer\PackageEvent $event
+     * @param Composer\Script\Event $event
      * @return string
      */
-    protected static function getSourceFile(Composer\Installer\PackageEvent $event) {
+    protected static function getSourceFile(Composer\Script\Event $event) {
         $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
         return $vendorDir . DIRECTORY_SEPARATOR . 'dcg' . DIRECTORY_SEPARATOR . 'dcg-lib-membership-number-state-client' . DIRECTORY_SEPARATOR . 'config.php';
     }
 
     /**
      * Get the location of where the config file should be copied to
-     * @param Composer\Installer\PackageEvent $event
+     * @param Composer\Script\Event $event
      * @return string
      */
-    protected static function getDestinationFile(Composer\Installer\PackageEvent $event) {
+    protected static function getDestinationFile(Composer\Script\Event $event) {
         $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
         return dirname($vendorDir) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'membership-number-state-config.php';
     }
